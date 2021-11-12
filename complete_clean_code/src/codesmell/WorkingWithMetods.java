962905ef00f879e4a28b57bf64f0dfeee0c6290a
Alper <alperyorgun2@gmail.com>
2021-11-12 14:19:02 +0300

    intel > m1?

--- complete_clean_code/src/codesmell/WorkingWithMetods.java
+++ complete_clean_code/src/codesmell/WorkingWithMetods.java
@@ -1,15 +1,81 @@
 package codesmell;
 
 public class WorkingWithMetods {
-    public static void main(String[] args) {
-        CustomerManager customerManager = new CustomerManager();
+    public static void main(String[] args) throws Exception {
+        CustomerDal customerDal = new CustomerDal();
+        CustomerManager customerManager = new CustomerManager(customerDal);
         customerManager.add(new Customer(1,2,"alper","yorgun","1234567890"));
 
     }
     static class CustomerManager{
-        public void add(Customer customer){
-            System.out.println("Eklendi.");
+        CustomerDal customerDal;
+        CustomerManager(CustomerDal customerDal){
+            this.customerDal = customerDal;
         }
+        public void add(Customer customer) throws Exception {
+            /*-
+            code smell
+            technical debt -> şimdilik kötü yazdım ama sonra onu düzelticem demek
+            if(!customer.firstName.isEmpty() && !customer.lastName.isEmpty() && !customer.identityNumber.isEmpty()){
+                // if bloğu ve altında sout bloğu asla birbiri ile temas etmemeli
+                System.out.println("Eklendi");
+            }
+             */
+
+            //doğru kod:
+            validateIdentityIfEmpty(customer);
+            validateFirstNameIfEmpty(customer);
+            validateLastNameIfEmpty(customer);
+            customerDal.add(customer);
+        }
+        public void addByOtherBusiness(Customer customer) throws Exception {
+            /*
+            bir nesneyi değer atarken diğer nesnelere nazaran farklı bir şekilde atanılması isteniyorsa
+            veya farklı bir şekilde iş katmanı yapılması isteniyorsa temel ekleme fonksiyonu değiştirmek yerine
+            ona özel farklı bir fonksiyon tanımlayarak bu sorun çözülebilir.
+            Böylelikle hem ad,soyad,tcno doğrulamasını çözmüş olduk
+            hemde bir nesne için özel istenen kuralı tanımlamış olduk
+             */
+            validateFirstNameLength(customer);
+            validateIdentityIfEmpty(customer);
+            validateFirstNameIfEmpty(customer);
+            validateLastNameIfEmpty(customer);
+            customerDal.add(customer);
+        }
+        private void validateFirstNameIfEmpty(Customer customer) throws  Exception{
+
+            if(customer.firstName.isEmpty()) {
+
+                throw new Exception("Validasyon hatası oldu");
+            }
+        }
+        private void validateLastNameIfEmpty(Customer customer) throws  Exception{
+
+            if(customer.lastName.isEmpty()) {
+
+                throw new Exception("Validasyon hatası oldu");
+            }
+        }
+        private void validateIdentityIfEmpty(Customer customer) throws  Exception{
+
+            if(customer.identityNumber.isEmpty()) {
+
+                throw new Exception("Validasyon hatası oldu");
+            }
+        }
+        private void validateFirstNameLength(Customer customer) throws  Exception{
+
+            if(customer.firstName.length() < 2) {
+
+                throw new Exception("Validasyon hatası oldu");
+            }
+        }
+    }
+    static class CustomerDal{
+        public void add(Customer customer){
+            System.out.println("Eklendi");
+        }
+
     }
      static class Customer{
         int cityId;
